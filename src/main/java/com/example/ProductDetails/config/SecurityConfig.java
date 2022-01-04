39eea2ec1c92f46623bfba3aef6f6c12c02bbc85
grigolenok <egor.grigolenok@mail.ru>
2022-01-04 18:01:25 +0300

    fix authorization

--- src/main/java/com/example/ProductDetails/config/SecurityConfig.java
+++ src/main/java/com/example/ProductDetails/config/SecurityConfig.java
@@ -1,11 +1,25 @@
 package com.example.ProductDetails.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        // super.configure(http);
+        http
+                .cors().and().csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic().disable()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
+
+
 }
--- src/main/java/com/example/ProductDetails/controller/ProductDetailsController.java
+++ src/main/java/com/example/ProductDetails/controller/ProductDetailsController.java
@@ -3,18 +3,15 @@ package com.example.ProductDetails.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class ProductDetailsController {
-
-    @Autowired
-    private AuthenticationManagerBuilder builder;
 
     @GetMapping("/test")
     public String test(){
-        builder.
-
-        return
+        return "works";
     }
 }
